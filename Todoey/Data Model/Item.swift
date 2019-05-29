9843ee41c2368b5ccc1e1ccad33b7a8b3a174f46
adrian <adrian.pena@solinftec.com.br>
2019-05-29 09:31:48 -0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Adrian Anselmo Del Toro Pena on 28/05/19.
+//  Copyright © 2019 Adrian Anselmo Del Toro Pena. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
